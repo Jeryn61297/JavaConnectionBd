a5092d1c9f41d7bc7585515d4abc7d7aa3729681
Kabec181 <Kabec181@10.115.12.184>
2024-04-08 18:46:04 -0500

    Codigo refactorizado

--- NewJava/src/main/java/com/jeska/EjemploJdbc.java
+++ NewJava/src/main/java/com/jeska/EjemploJdbc.java
@@ -14,19 +14,24 @@ public class EjemploJdbc {
 		//String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
 		String username = "root";
 		String password = "sasa";
-		try {
-			Connection conn = DriverManager.getConnection(url, username, password); 
+		
+		try (Connection conn = DriverManager.getConnection(url, username, password); 
 			Statement stmt = conn.createStatement();
-			ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+			ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")) {
 			
 			while (resultado.next()) {
-				System.out.println(resultado.getString("nombre"));
+				System.out.print(resultado.getInt("id"));
+				System.out.print(" | ");
+				System.out.print(resultado.getString("nombre"));
+				System.out.print(" | ");
+				System.out.print("$");
+				System.out.print(resultado.getInt("precio"));
+				System.out.print(" | ");
+				System.out.print(resultado.getDate("fecha_registro"));
+				System.out.println(" ");
 			}
-			resultado.close();
-			stmt.close();
-			conn.close();
-		}catch (SQLException throwables) {
-			throwables.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
