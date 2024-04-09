a855346f97f2efae45cc23148b6bc01a1e956552
Kabec181 <Kabec181@10.115.12.184>
2024-04-08 20:08:31 -0500

    Producto y interfas

---
+++ NewJava/src/main/java/com/jeska/repositorio/Repositorio.java
@@ -0,0 +1,16 @@
+package com.jeska.repositorio;
+
+import java.util.List;
+
+public interface Repositorio<T> {
+
+	List<T> listar();
+
+	T porId(Long id);
+
+	void guardar(T t);
+
+	void eliminar(Long id);
+
+}
+
