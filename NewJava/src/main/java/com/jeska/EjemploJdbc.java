610caddf2dc0a40dec5a9a48ca5c6febfb072e72
Kabec181 <Kabec181@10.115.12.184>
2024-04-08 19:17:24 -0500

    Conexion BD

--- NewJava/src/main/java/com/jeska/EjemploJdbc.java
+++ NewJava/src/main/java/com/jeska/EjemploJdbc.java
@@ -6,16 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.jeska.util.ConexionBaseDatos;
+
 public class EjemploJdbc {
 	
 	public static void main(String[] args) {
 		
-		String url = "jdbc:mysql://localhost:3306/java_curso";
-		//String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
-		String username = "root";
-		String password = "sasa";
-		
-		try (Connection conn = DriverManager.getConnection(url, username, password); 
+		try (Connection conn = ConexionBaseDatos.getInstance(); 
 			Statement stmt = conn.createStatement();
 			ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")) {
 			
